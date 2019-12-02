a8d923d7d9f74b445bc91c137a69509d75c54648
Mina <mkovace5@uwo.ca>
2019-12-01 19:14:11 -0500

    can click on song to bring you to different page

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { HomeComponent } from './home/home.component';
 import { CallbackComponent } from './callback/callback.component';
 import { SongsComponent } from './songs/songs.component';
 import { ReviewComponent } from './review/review.component';
+import { SongReviewComponent } from './song-review/song-review.component';
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent },
   {path: 'callback', component: CallbackComponent},
   {path: 'songs', component: SongsComponent},
-  {path: 'review', component: ReviewComponent}
+  {path: 'review', component: ReviewComponent},
+  {path: 'song-review', component: SongReviewComponent}
 ];
 
 @NgModule({
